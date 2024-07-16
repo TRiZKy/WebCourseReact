133b4c6f06caa7c32c4e2ecf797f67c0cbc2df6a
Shay Bistrizky <shaybis.sb@gmail.com>
2024-07-16 23:42:00 +0300

    fix of firestore api 3

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import connectDB from '../config/db.js';
-import sensorRoutes from '../routes/sensorRoutes.js';
-import cropRoutes from '../routes/cropRoutes.js';
+import connectDB from './config/db.js';
+import sensorRoutes from './routes/sensorRoutes.js';
+import cropRoutes from './routes/cropRoutes.js';
 
 dotenv.config();
 
@@ -16,11 +16,20 @@ const app = express();
 const corsOptions = {
     origin: process.env.FRONTEND_URL,
     optionsSuccessStatus: 200,
+    credentials: true,
 };
 
 // Middleware
 app.use(cors(corsOptions));
 app.use(express.json());
+
+// Manually set CORS headers for all requests
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 
 // Routes
 app.use('/api/sensors', sensorRoutes);
