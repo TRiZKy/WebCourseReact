ea23c1edec4da0b666cc3b03290bb8e9555cfb50
Shay Bistrizky <shaybis.sb@gmail.com>
2024-07-16 22:11:32 +0300

    fix of firestore api

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -5,8 +5,11 @@ dotenv.config();
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
+// Handle escaping issues for private key
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
 });
 
 export default admin;
